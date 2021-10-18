8bffb4d41c8cfe9bf25cc3e984a9b24bfb992f89
Nguyễn Minh An <nguyenminhan@codelovers.vn>
2021-10-18 10:49:26 +0700

    fix

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -1,62 +1,118 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: !firstName.isValid}">
       <label for="firstname">Firstname</label>
-      <input type="text" id="firstname" v-model.trim="firstName" />
+      <input type="text" id="firstname" v-model.trim="firstName.val" @blur="clearValidity('firstName')" />
+      <p v-if="!firstName.isValid">Firstname must not be empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: !lastName.isValid}">
       <label for="lastname">Lastname</label>
-      <input type="text" id="lastname" v-model.trim="lastName" />
+      <input type="text" id="lastname" v-model.trim="lastName.val" @blur="clearValidity('lastName')" />
+      <p v-if="!lastName.isValid">Lastname must not be empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: !description.isValid}">
       <label for="description">Description</label>
-      <textarea id="description" rows="5" v-model.trim="description"></textarea>
+      <textarea id="description" rows="5" v-model.trim="description.val" @blur="clearValidity('description')"></textarea>
+      <p v-if="!description.isValid">Description must not be empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: !rate.isValid}">
       <label for="rate">Hourly Rate</label>
-      <input type="number" id="rate" v-model.number="rate" />
+      <input type="number" id="rate" v-model.number="rate.val" @blur="clearValidity('rate')" />
+      <p v-if="!rate.isValid">Rate must be greater than 0.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: !areas.isValid}">
       <h3>Areas of Expertise</h3>
       <div>
-        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
+        <input type="checkbox" id="frontend" value="frontend" v-model="areas.val" @blur="clearValidity('areas')"  />
         <label for="frontend">Frontend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="backend" value="backend" v-model="areas" />
+        <input type="checkbox" id="backend" value="backend" v-model="areas.val" @blur="clearValidity('areas')" />
         <label for="backend">Backend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="career" value="career" v-model="areas" />
+        <input type="checkbox" id="career" value="career" v-model="areas.val" @blur="clearValidity('areas')" />
         <label for="career">Career Advisory</label>
+        <p v-if="!areas.isValid">At least one expertise must be selected.</p>
       </div>
+      <p v-if="!formIsValid">Please fix the above errors and submit again.</p>
     </div>
     <base-button>Register</base-button>
   </form>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
 export default {
   setup(_, { emit }) {
-    const firstName = ref("");
-    const lastName = ref("");
-    const description = ref("");
-    const rate = ref(null);
-    const areas = ref([]);
+    const firstName = reactive({
+      val: '',
+      isValid: true
+    });
+    const lastName = reactive({
+      val: '',
+      isValid: true
+    });
+    const description = reactive({
+      val: '',
+      isValid: true
+    });
+    const rate = reactive({
+      val: null,
+      isValid: true,
+    });
+    const areas = reactive({
+      val: [],
+      isValid: true,
+    });
+
+    const formIsValid = ref(true);
+
+    function clearValidity(input) {
+      console.log(input);
+      [input].isValid = true;
+    }
+
+    function validateForm() {
+      formIsValid.value = true;
+      if(firstName.val === ''){
+        firstName.isValid = false;
+        formIsValid.value = false;
+      }
+      if(lastName.val === ''){
+        lastName.isValid = false;
+        formIsValid.value = false;
+      }
+      if(description.val === ''){
+        description.isValid = false;
+        formIsValid.value = false;
+      }
+      if(!rate.val || rate.val < 0){
+        rate.isValid = false;
+        formIsValid.value = false;
+      }
+      if(areas.val.length === 0){
+        areas.isValid = false;
+        formIsValid.value = false;
+      }
+    }
 
     function submitForm() {
+      validateForm();
+      if (!formIsValid.value) {
+        return;
+      }
       const formData = {
-        first: firstName.value,
-        last: lastName.value,
-        desc: description.value,
-        rate: rate.value,
-        areas: areas.value,
+        first: firstName.val,
+        last: lastName.val,
+        desc: description.val,
+        rate: rate.val,
+        areas: areas.val,
       };
       emit('save-data', formData)
     }
-    return { firstName, lastName, description, rate, areas, submitForm };
+    return { firstName, lastName, description, rate, areas, submitForm, formIsValid, clearValidity };
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,20 @@
 import { createStore } from 'vuex';
 import coachesModule from './modules/coaches';
+import requestsModule from './modules/requests';
 const store = createStore({
     modules: {
-        coaches: coachesModule
+        coaches: coachesModule,
+        requests: requestsModule
+    },
+    state() {
+        return {
+            userId: 'c3'
+        };
+    },
+    getters: {
+        userId(state) {
+            return state.userId;
+        }
     }
 });
 export default store;
